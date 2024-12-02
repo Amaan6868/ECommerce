575845448cfb5073af53870efb1d94f8a75088c4
Amaan6868 <2258069@johnabbottcollege.net>
2024-12-02 16:17:22 -0500

    Implemented code for Products project

---
+++ ECommerce.Api.Products/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    /*Course: 		Web Programming 3
+    * Assessment: 	Milestone 4
+    * Created by: 	Amaan Afzal - 2258069
+    * Date: 		<01> <12> 2024
+    * Class Name: 	ProductsController.cs
+    * Description: 	Fetches Product data from their id or just all products
+    * Time for Task:5 hours
+    */
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsProvider productsProvider;
+
+        public ProductsController(IProductsProvider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var result = await productsProvider.GetProductsAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Products);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id)
+        {
+            var result = await productsProvider.GetProductAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Product);
+            }
+            return NotFound();
+        }
+    }
+}
