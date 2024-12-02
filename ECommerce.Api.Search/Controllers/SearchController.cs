fcacf2c916227d64f18cb8512300fb99a631e463
Amaan6868 <2258069@johnabbottcollege.net>
2024-12-02 16:56:08 -0500

    Implmented Search project

---
+++ ECommerce.Api.Search/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Controllers
+{
+/*Course: 		Web Programming 3
+* Assessment: 	Milestone 4
+* Created by: 	Amaan Afzal - 2258069
+* Date: 		<01> <12> 2024
+* Class Name: 	SearchController.cs
+* Description: 	Fetches all data from a customerId
+* Time for Task:5 hours
+*/
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        private readonly ISearchService searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchTerm term)
+        {
+            var result = await searchService.SearchAsync(term.CustomerId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.SearchResults);
+            }
+            return NotFound();
+        }
+    }
+}
